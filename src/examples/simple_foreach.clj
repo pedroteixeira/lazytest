42682a6dbe3aa603848a22c83d7b44d0998d50eb
Stuart Sierra <mail@stuartsierra.com>
2010-07-02 14:45:03 -0400

    Add assertions for for-each

---
+++ src/examples/simple_foreach.clj
@@ -0,0 +1,9 @@
+(ns simple-foreach
+  (:use lazytest.describe))
+
+(describe + "with integers"
+  (for-each "should add small numbers"
+	    [a b sum] (= sum (+ a b))
+	    [3 4 7]
+	    [2 2 4]
+	    [5 5 1000]))
--- src/scripts/runtests.clj
+++ src/scripts/runtests.clj
@@ -29,4 +29,29 @@
   (let [example-result (first (:children group-results))
 	example-meta (meta (:source example-result))]
     (assert (not (success? example-result)))))
+;(assert (= 10 (:expected example-result)))
+;(assert (= 7 (:actual example-result)))
 
+(println "Testing simple-foreach ...")
+(remove-ns 'simple-foreach)
+(load "src/examples/simple_foreach")
+(let [group-results (run-tests (first (groups 'simple-foreach)))
+      group-meta (meta (:source group-results))]
+  (assert (not (success? group-results)))
+  (let [mapping-results (first (:children group-results))
+	mapping-meta (meta (:source mapping-results))]
+    (assert (not (success? mapping-results)))
+    (let [example-result (first (:children mapping-results))
+	  example-meta (meta (:source example-result))]
+      (assert (success? example-result))
+      (assert (= (list 3 4 7) (:states example-result))))
+    (let [example-result (last (:children mapping-results))
+	  example-meta (meta (:source example-result))]
+      (assert (not (success? example-result)))
+      (assert (= (list 5 5 1000) (:states example-result)))
+      (assert (not (container? example-result)))
+      (assert (integer? (:line example-meta)))
+      (assert (= 5 (:line example-meta)))
+      (assert (string? (:doc example-meta)))
+      (assert (re-matches #".*clojure\.core/\+ with integers should add small numbers.*"
+			  (:doc example-meta))))))
