c505a6766cb8a9c083c775457a3bdb8a1916359f
Stuart Sierra <mail@stuartsierra.com>
2010-07-02 21:27:11 -0400

    Remove all TestResult methods but 'success?', make ordinary fns

--- src/main/clojure/lazytest/results.clj
+++ src/main/clojure/lazytest/results.clj
@@ -5,83 +5,51 @@
   pending."
   (:use [lazytest.arguments :only (nil-or)]))
 
+;;; PROTOCOLS
+
 (defprotocol TestResult
-  (success? [r] "True if this result and all its children passed.")
-  (pending? [r] "True if this is the result of an empty test.")
-  (error? [r] "True if this is the result of a thrown exception.")
-  (skipped? [r] "True if this is the result of a skipped test.")
-  (container? [r] "True if this is a container for other results."))
+  (success? [r] "True if this result and all its children passed."))
+
+;;; TEST RESULT TYPES
 
 (defrecord TestResultContainer [source children]
   TestResult
-    (success? [this] (every? success? children))
-    (pending? [this] (if (seq children) false true))
-    (error? [this] false)
-    (skipped? [this] false)
-    (container? [this] true))
-
-(defrecord TestPassed [source states]
-  TestResult
-    (success? [this] true)
-    (pending? [this] false)
-    (error? [this] false)
-    (skipped? [this] false)
-    (container? [this] false))
-
-(defrecord TestFailed [source states reason]
-  TestResult
-    (success? [this] false)
-    (pending? [this] false)
-    (error? [this] false)
-    (skipped? [this] false)
-    (container? [this] false))
-
-(defrecord TestThrown [source states throwable]
-  TestResult
-    (success? [this] false)
-    (pending? [this] false)
-    (error? [this] true)
-    (skipped? [this] false)
-    (container? [this] false))
-
-(defrecord TestSkipped [source reason]
-  TestResult
-    (success? [this] true)
-    (pending? [this] false)
-    (error? [this] false)
-    (skipped? [this] true)
-    (container? [this] false))
-
-(defrecord TestPending [source reason]
-  TestResult
-    (success? [this] true)
-    (pending? [this] true)
-    (error? [this] false)
-    (skipped? [this] false)
-    (container? [this] false))
-
-(defrecord TestResultContainer [source children]
-  TestResult
-  (success? [this] (every? success? children))
-  (pending? [this] false)
-  (error? [this] false)
-  (skipped? [this] false)
-  (container? [this] true))
-
-(defrecord NotEqualFailure [expected actual])
+    (success? [this] (every? success? children)))
 
 (defn container [source children]
   (TestResultContainer. source children))
 
+(defn container? [x]
+  (instance? TestResultContainer x))
+
+(defrecord TestPassed [source states]
+  TestResult
+    (success? [this] true))
+
 (defn pass [source states]
   (TestPassed. source states))
+
+(defrecord TestFailed [source states reason]
+  TestResult
+    (success? [this] false))
 
 (defn fail [source states reason]
   (TestFailed. source states reason))
 
+(defrecord TestThrown [source states throwable]
+  TestResult
+    (success? [this] false))
+
 (defn thrown [source states throwable]
   {:pre [(instance? Throwable throwable)]}
   (TestThrown. source states throwable))
+
+(defn error? [x]
+  (instance? TestThrown x))
+
+(defrecord TestSkipped [source reason]
+  TestResult
+    (success? [this] true))
 
 (defn skip
   ([source] (skip source nil))
@@ -89,11 +57,25 @@
      {:pre [(nil-or string? reason)]}
      (TestSkipped. source reason)))
 
+(defn skipped? [x]
+  (instance? TestSkipped x))
+
+(defrecord TestPending [source reason]
+  TestResult
+    (success? [this] true))
+
 (defn pending
   ([source] (pending source nil))
   ([source reason]
      {:pre [(nil-or string? reason)]}
      (TestPending. source reason)))
+
+(defn pending? [x]
+  (instance? TestPending x))
+
+;;; FAILURE REASON TYPES
+
+(defrecord NotEqualFailure [expected actual])
 
 (defn not-equal [expected actual]
   (NotEqualFailure. expected actual))
